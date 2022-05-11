e0b2ef05007778007330f2621f9beb1365f676de
behnam1369 <behnam.aghaali@yahoo.com>
2022-05-12 00:58:31 +0430

    Add rent method to the Book class

--- book.rb
+++ book.rb
@@ -1,8 +1,16 @@
 class Book
+  attr_reader :rental_history
   attr_accessor :title, :author
 
   def initialize(title, author)
     @title = title
     @author = author
+    @rental_history = []
+  end
+
+  def rent(date, person)
+    return if @rental_history[@rental_history.length - 1].book == self
+
+    Rental.new(date, self, person)
   end
 end
