def92e3d73e1cc9d4caabe3f81a6ea8b4b30d3d0
behnam1369 <behnam.aghaali@yahoo.com>
2022-05-11 23:57:14 +0430

    Add belongs_to method for Student Class

--- student.rb
+++ student.rb
@@ -1,6 +1,8 @@
 require './person'
 
 class Student < Person
+  attr_accessor :classroom
+
   def initialize(age, classroom, name = 'Unknown', parent_permission = nil)
     super(age, name, parent_permission)
     @classroom = classroom
@@ -8,6 +10,16 @@ class Student < Person
 
   def play_hooky
     "¯\(ツ)/¯"
+  end
+
+  def belongs_to(classroom)
+    return if self.classroom == classroom
+
+    @classroom = classroom
+
+    return if classroom.student_list.include? self
+
+    classroom.student_list.push(self)
   end
 end
 
@@ -19,3 +31,4 @@ end
 
 # s3 = Student.new(16, "B")
 # p s3
+
